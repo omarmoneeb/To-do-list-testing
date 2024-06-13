d470b304189eb7e5138102b84b679b905c41b05f
omarmoneeb <Omarmoneeb1998@gmail.com>
2024-06-13 13:41:46 -0300

    Adding to the list is working now.

--- src/components/TodoApp.tsx
+++ src/components/TodoApp.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 interface Todo {
   id: string;
   text: string;
@@ -8,14 +8,56 @@ interface Todo {
 const TodoApp = () => {
   const [todos, setTodos] = useState<Todo[]>([]);
   const [inputText, setInputText] = useState("");
+  function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    if (inputText.trim() === "") return;
+    const newTodo: Todo = {
+      id: crypto.randomUUID(),
+      text: inputText,
+      isDone: false,
+    };
+    setTodos([...todos, newTodo]);
+    setInputText("");
+  }
+
+  function handleToggleTodo(id: string) {
+    const updatedTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        return {
+          ...todo,
+          isDone: !todo.isDone,
+        };
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  }
 
   return (
     <div>
       <h1>Todo App</h1>
-      <label htmlFor="todo"></label>
-      <input type="text" id="todo" value={inputText} />
-      <button> Add Todo</button>
-      <ul></ul>
+      <form onClick={handleSubmit}>
+        <label htmlFor="todo"></label>
+        <input
+          type="text"
+          id="todo"
+          value={inputText}
+          onChange={(e) => setInputText(e.target.value)}
+        />
+        <button> Add Todo</button>
+      </form>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            <span
+              style={{ textDecoration: todo.isDone ? "line-through" : "none" }}
+              onClick={() => handleToggleTodo(todo.id)}
+            >
+              {todo.text}
+            </span>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
